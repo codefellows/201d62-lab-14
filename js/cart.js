82a0be58ff25b3603ff1b6f5dfda1670116688a4
Melissa Galush <galushmn@gmail.com>
2020-04-16 15:02:15 -0700

    melissa finished driving

--- js/cart.js
+++ js/cart.js
@@ -21,16 +21,33 @@ function renderCart() {
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
 function clearCart() {}
 
-// TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
+// DONE: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
-  // TODO: Find the table body
+  // DONE: Find the table body
+  var targetBody = document.getElementsByTagName('tbody')[0];
+  targetBody.id = 'test';
+  // DONE: Iterate over the items in the cart
+  for( var i = 0; i < cart.items.length; i++ ) {
+    // DONE: Create a TR
+    var newTr = document.createElement('tr');
+    newTr.id = cart.items[i].product;
+    // DONE: Create a TD for the delete link, quantity,  and the item
+    // DONE: Add the TR to the TBODY and each of the TD's to the TR
+    var newTdDelete = document.createElement('td');
+    newTdDelete.textContent = 'X';
+    newTdDelete.id = cart.items[i].product + 'td';
+    newTr.appendChild(newTdDelete);
 
-  // TODO: Iterate over the items in the cart
-  // TODO: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
-  // TODO: Add the TR to the TBODY and each of the TD's to the TR
+    var newTdQuantity = document.createElement('td');
+    newTdQuantity.textContent = cart.items[i].quantity;
+    newTr.appendChild(newTdQuantity);
 
+    var newTdItem = document.createElement('td');
+    newTdItem.textContent = cart.items[i].product;
+    newTr.appendChild(newTdItem);
+  }
+  targetBody.appendChild(newTr);
 }
 
 function removeItemFromCart(event) {
